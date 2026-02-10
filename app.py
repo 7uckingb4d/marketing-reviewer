8c814977f3996fcf676577f2ef297ce7b5ab784a
Stephen Rey Bulahan <stephenreybulahan@plv.edu.ph>
2026-02-10 10:50:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ else:
 try:
     if "GEMINI_API_KEY" in st.secrets:
         genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-        model = genai.GenerativeModel('gemini-1.5-flash')
+        model = genai.GenerativeModel('gemini-pro')
     else:
         st.error("⚠️ Wala pang API Key. Ilagay ito sa Streamlit Secrets.")
         st.stop()
